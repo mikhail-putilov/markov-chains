dccd206e2301acca9e8cca39a8ab7ecbbd929d4c
Mikhail Putilov <post.snowy@gmail.com>
2016-08-28 21:38:09 +0300

    Remove music. Add poem generation

--- main.py
+++ main.py
@@ -5,6 +5,30 @@ from pymarkovchain import MarkovChain
 # store and load its database files to. You probably want to give it another location, like so:
 mc = MarkovChain()
 # To generate the markov chain's language model, in case it's not present
-mc.generateDatabase("I am Skrillex. I am Misha. I am Natasha. I am govnuk ")
+mc.generateDatabase("""Angel
+
+By gates of Eden, Angel, gentle,
+Shone with his softly drooped head,
+And Demon, gloomy and resentful
+Over the hellish crevasse flapped.
+
+The spirit of qualm and negation
+Looked at another one – of good,
+And fire of the forced elation
+First time he vaguely understood.
+
+“I’ve seen you,” he enunciated, -
+“And not in vain you’ve sent me light:
+Not all in heaven I have hated,
+Not all in world I have despised.”""")
 # To let the markov chain generate some text, execute
 print(mc.generateString())
+print(mc.generateString())
+print(mc.generateString())
+print(mc.generateString())
+
+# result:
+# Shone with his softly drooped head,
+# First time he vaguely understood
+# The spirit of qualm and negation
+# And fire of the forced elation
